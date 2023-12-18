81cdec41074b00490ddd3b2cfced13b2e662aabc
Fabrizzio Ontaneda <faontane@espol.edu.ec>
2023-12-18 12:19:06 -0500

    Cambios visuales en la matriz de asistencia

--- diakonia-admin-web/src/App.js
+++ diakonia-admin-web/src/App.js
@@ -45,7 +45,7 @@ function Aplicacion() {
       almuerzo: false,
     },
     {
-      id:2,
+      id: 2,
       nombre: 'Institución 2',
       telefono: '987-654-3210',
       ubicacion: 'Ciudad B',
@@ -59,7 +59,7 @@ function Aplicacion() {
       id: 1,
       institucionId: 1,
       nombre: 'benf 1',
-      edad: 15,  
+      edad: 15,
     },
     {
       id: 3,
@@ -68,13 +68,13 @@ function Aplicacion() {
       edad: 16,
     },
     {
-      id:2,
+      id: 2,
       nombre: 'benf 2',
       institucionId: 2,
       edad: 8,
-      
+
     },
-   
+
   ]);
 
   const agregarBeneficiario = (nuevoBeneficiario) => {
@@ -86,139 +86,180 @@ function Aplicacion() {
       nombre: 'pepe',
       contraseña: '12345',
       usuario: 'user 1',
-      
+
     },
     {
       id: 3,
       nombre: 'ana',
       contraseña: '54321',
       usuario: 'user 3',
-      
+
     },
     {
-      id:2,
+      id: 2,
       nombre: 'titi',
       contraseña: '45321',
       usuario: 'user 2',
-      
+
     },
-   
+
   ];
-  
+
   const asistencias = [
+    {
+      id: 1,
+      nombre: 'benf 1',
+      instituciones: [
+        {
+          id: 1,
+          asistencias: [
+            { mes: 'enero', dias: ['24', '25', '26', '27'], cantidad: 4 },
+            { mes: 'febrero', dias: ['28'], cantidad: 1 },
+            { mes: 'marzo', dias: ['01'], cantidad: 1 },
+            { mes: 'diciembre', dias: ['01', '02', '03'], cantidad: 3 },
+          ],
+        },
+        {
+          id: 2,
+          asistencias: [
+            { mes: 'febrero', dias: ['29'], cantidad: 1 },
+            { mes: 'marzo', dias: [], cantidad: 0 },
+            { mes: 'diciembre', dias: ['30', '31'], cantidad: 2 },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      nombre: 'benf 2',
+      instituciones: [
+        {
+          id: 1,
+          asistencias: [
+            { mes: 'febrero', dias: ['24', '25', '26'], cantidad: 3 },
+            // Continuar para los meses siguientes...
+            { mes: 'diciembre', dias: ['01', '02'], cantidad: 2 },
+          ],
+        },
+        {
+          id: 2,
+          asistencias: [
+            { mes: 'febrero', dias: ['27'], cantidad: 1 },
+            { mes: 'marzo', dias: ['28', '29'], cantidad: 2 },
+            // Continuar para los meses siguientes...
+            { mes: 'diciembre', dias: ['03', '04'], cantidad: 2 },
+          ],
+        },
+      ],
+    },
+    {
+      id: 3,
+      nombre: 'benf 3',
+      instituciones: [
+        {
+          id: 1,
+          asistencias: [
+            { mes: 'enero', dias: ['24', '25', '26'], cantidad: 3 },
+            { mes: 'febrero', dias: ['27', '28'], cantidad: 2 },
+            { mes: 'marzo', dias: ['01'], cantidad: 1 },
+            // Continuar para los meses siguientes...
+            { mes: 'diciembre', dias: ['02', '03'], cantidad: 2 },
+          ],
+        },
+      ],
+    },
+    // Continuar para más beneficiarios...
+  ];
+
+
+  const nutricion = [
     {
       id: 1,
       institucionId: 1,
       nombre: 'benf 1',
-      asistencias: '4',
-      lista: ['24-ene','25-ene','26-ene','27-ene'],
-      
+      imc: '40',
+      peso: '45',
+
     },
     {
       id: 3,
       institucionId: 1,
       nombre: 'benf 3',
-      asistencias: '3',
-      lista: ['24-ene','25-ene','26-ene'],
-      
+      imc: '40',
+      peso: '45',
+
     },
     {
-      id:2,
+      id: 2,
       institucionId: 2,
       nombre: 'benf 2',
-      asistencias: '4',
-      lista: ['24-ene','25-ene','26-ene','27-ene'],
+      imc: '40',
+      peso: '45',
     },
-   
   ];
-  const nutricion = [
-    {
-    id: 1,
-    institucionId: 1,
-    nombre: 'benf 1',
-    imc: '40',
-    peso: '45',
-    
-  },
-  {
-    id: 3,
-    institucionId: 1,
-    nombre: 'benf 3',
-    imc: '40',
-    peso: '45',
-    
-  },
-  {
-    id:2,
-    institucionId: 2,
-    nombre: 'benf 2',
-    imc: '40',
-    peso: '45',
-  },
- ];
 
- const [user,setUser] = useState(null);
- 
- 
- async function getRol(uid) {
-  const docuRef = doc(firestore, `usuarios/${uid}`);
-  const docuCifrada = await getDoc(docuRef);
-  const infoFinal = docuCifrada.data().rol;
-  return infoFinal;
-}
+  const [user, setUser] = useState(null);
 
-function setUserWithFirebaseAndRol(usuarioFirebase) {
-  getRol(usuarioFirebase.uid).then((rol) => {
-    const userData = {
-      uid: usuarioFirebase.uid,
-      email: usuarioFirebase.email,
-      rol: rol,
-    };
-    setUser(userData);
-  });
-}
 
- 
- onAuthStateChanged(auth, (usuarioFirebase) => {
-  if (usuarioFirebase) {
-    //funcion final
-    
-    if (!user) {  
-      //setUser(usuarioFirebase);
-      setUserWithFirebaseAndRol(usuarioFirebase);
-    }
-  } else {
-    setUser(null);
+  async function getRol(uid) {
+    const docuRef = doc(firestore, `usuarios/${uid}`);
+    const docuCifrada = await getDoc(docuRef);
+    const infoFinal = docuCifrada.data().rol;
+    return infoFinal;
   }
-});
+
+  function setUserWithFirebaseAndRol(usuarioFirebase) {
+    getRol(usuarioFirebase.uid).then((rol) => {
+      const userData = {
+        uid: usuarioFirebase.uid,
+        email: usuarioFirebase.email,
+        rol: rol,
+      };
+      setUser(userData);
+    });
+  }
+
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      //funcion final
+
+      if (!user) {
+        //setUser(usuarioFirebase);
+        setUserWithFirebaseAndRol(usuarioFirebase);
+      }
+    } else {
+      setUser(null);
+    }
+  });
 
 
   return (
     <div className="Aplicacion">
 
-{user === null ? <Login user={user} setUser={setUser}/> : 
-      <Routes>
-        
+      {user === null ? <Login user={user} setUser={setUser} /> :
+        <Routes>
 
-        <Route path="/" element={ <Inicio /> } />
-        <Route path="instituciones" element={ <Instituciones /> } />
-        <Route path="beneficiarios" element={ <Beneficiarios instituciones={instituciones} /> } />
-        <Route path="seguimiento" element={ <Seguimiento /> } />
-        <Route path="usuarios" element={ <Usuarios /> } />
-        <Route path="registrar" element={ <Registrar /> } />
-        <Route path="verInstitucion" element={ <VerInstitucion instituciones={instituciones}/> } />
-        <Route path="beneficiarios/:institucionId/:institucionN" element={<ListaBeneficiarios instituciones={instituciones}  beneficiarios={beneficiarios} />} />
-        <Route path="editar-beneficiario/:institucionId/:beneficiarioid" element={<EditarBeneficiario instituciones={instituciones}  beneficiarios={beneficiarios} />} />
-        <Route path="registrarUsuario" element={ <RegistroUsuario /> } />
-        <Route path="verUsuarios" element={ <VerUsuarios usuarios={usuarios}/> } />
-        <Route path="asistencias" element={ <VerAsistencias instituciones={instituciones} /> } />
-        <Route path="asistencias/:institucionId" element={<ListaAsistencias instituciones={instituciones}  asistencias={asistencias} />} />
-        <Route path="nutricion" element={ <Nutricion instituciones={instituciones} /> } />
-        <Route path="nutricion/:institucionId" element={<ListaNutricion instituciones={instituciones}  nutricion={nutricion} />} />
-        <Route path="verGrafica/:institucionId/:beneficiarioid" element={<VerGrafica nutricion={nutricion} />} />
-        <Route path="beneficiarios/:institucionId/:institucionN/añadirbenef" element={ <LeerExcel beneficiarios={beneficiarios} agregarBeneficiario = {agregarBeneficiario}/> } />
-      </Routes>
-}
+
+          <Route path="/" element={<Inicio />} />
+          <Route path="instituciones" element={<Instituciones />} />
+          <Route path="beneficiarios" element={<Beneficiarios instituciones={instituciones} />} />
+          <Route path="seguimiento" element={<Seguimiento />} />
+          <Route path="usuarios" element={<Usuarios />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="verInstitucion" element={<VerInstitucion instituciones={instituciones} />} />
+          <Route path="beneficiarios/:institucionId/:institucionN" element={<ListaBeneficiarios instituciones={instituciones} beneficiarios={beneficiarios} />} />
+          <Route path="editar-beneficiario/:institucionId/:beneficiarioid" element={<EditarBeneficiario instituciones={instituciones} beneficiarios={beneficiarios} />} />
+          <Route path="registrarUsuario" element={<RegistroUsuario />} />
+          <Route path="verUsuarios" element={<VerUsuarios usuarios={usuarios} />} />
+          <Route path="asistencias" element={<VerAsistencias instituciones={instituciones} />} />
+          <Route path="asistencias/:institucionId" element={<ListaAsistencias instituciones={instituciones} asistencias={asistencias} />} />
+          <Route path="nutricion" element={<Nutricion instituciones={instituciones} />} />
+          <Route path="nutricion/:institucionId" element={<ListaNutricion instituciones={instituciones} nutricion={nutricion} />} />
+          <Route path="verGrafica/:institucionId/:beneficiarioid" element={<VerGrafica nutricion={nutricion} />} />
+          <Route path="beneficiarios/:institucionId/:institucionN/añadirbenef" element={<LeerExcel beneficiarios={beneficiarios} agregarBeneficiario={agregarBeneficiario} />} />
+        </Routes>
+      }
     </div>
   )
 }
--- diakonia-admin-web/src/components/ListaAsistencias.js
+++ diakonia-admin-web/src/components/ListaAsistencias.js
@@ -1,39 +1,106 @@
-import React from 'react';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Cabecera from './Cabecera';
-import Popup from './Popup';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import '../estilos/ListaAsistencias.css';
+import Cabecera from './Cabecera';
 
-const ListaBeneficiarios = ({ instituciones, asistencias }) => {
-  const { institucionId } = useParams();
-  // Encuentra la institución seleccionada por su ID
-  const institucionSeleccionada = instituciones.find((inst) => inst.id === parseInt(institucionId, 10));
 
-  // Muestra la lista de beneficiarios de la institución seleccionada
-  const beneficiariosDeInstitucion = asistencias.filter(
-    (asistencias) => asistencias.institucionId === institucionSeleccionada.id
+const ListaAsistencias = ({ asistencias }) => {
+  // Obtener la lista única de meses
+  const mesesUnicos = Array.from(
+    new Set(
+      asistencias.flatMap((beneficiario) =>
+        beneficiario.instituciones.flatMap((institucion) =>
+          institucion.asistencias.map((asistencia) => asistencia.mes)
+        )
+      )
+    )
   );
+
+  // Estado local para rastrear el mes seleccionado (inicializado en "enero")
+  const [mesSeleccionado, setMesSeleccionado] = useState('enero');
+  const [diasDelMes, setDiasDelMes] = useState([]);
+
+  // Actualizar los días del mes cuando se cambia el mes seleccionado
+  useEffect(() => {
+    if (mesSeleccionado) {
+      const diasUnicos = Array.from(
+        new Set(
+          asistencias.flatMap((beneficiario) =>
+            beneficiario.instituciones.flatMap((institucion) =>
+              institucion.asistencias
+                .filter((asistencia) => asistencia.mes === mesSeleccionado)
+                .flatMap((asistencia) => asistencia.dias)
+            )
+          )
+        )
+      );
+      setDiasDelMes(diasUnicos);
+    } else {
+      setDiasDelMes([]);
+    }
+  }, [mesSeleccionado, asistencias]);
 
   return (
     <div className="centered-container">
-     <Cabecera/>
-      <h1>Lista de Asistencias de {institucionSeleccionada.nombre}</h1>
-      {/* Aquí deberías mostrar la lista de beneficiarios de la institución */}
-      <ul>
-        {beneficiariosDeInstitucion.map((beneficiario) => (
-          <li key={beneficiario.id}>
-            {beneficiario.nombre} - {beneficiario.asistencias} asistencias - {beneficiario.id} id - {beneficiario.lista.join(', ')}
-            {/*{<Link to={`/editar-beneficiario/${institucionSeleccionada.id}/${beneficiario.id}`}>Editar</Link>}*/}
-          
-            {/* Puedes mostrar más información sobre cada beneficiario según tu estructura de datos */}
-          </li>
-        ))}
-      </ul>
+      <Cabecera />
+      <h1>Asistencias</h1>
+
+       <label>
+        Filtrar por mes:
+        <select
+          value={mesSeleccionado}
+          onChange={(e) => setMesSeleccionado(e.target.value)}
+        >
+          {mesesUnicos.map((mes, index) => (
+            <option key={index} value={mes}>
+              {mes}
+            </option>
+          ))}
+        </select>
+      </label>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Beneficiario</th>
+            {diasDelMes.map((dia, index) => (
+              <th key={index}>{dia}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {asistencias.map((beneficiario) => (
+            <tr key={beneficiario.id}>
+              <td>{beneficiario.nombre}</td>
+              {diasDelMes.map((dia, index) => {
+                const asistio = beneficiario.instituciones.some(
+                  (institucion) =>
+                    institucion.asistencias
+                      .filter(
+                        (asistencia) =>
+                          asistencia.mes === mesSeleccionado &&
+                          asistencia.dias.includes(dia)
+                      )
+                      .reduce(
+                        (total, asistencia) => total + asistencia.cantidad,
+                        0
+                      ) > 0
+                );
+
+                return (
+                  <td
+                    key={index}
+                    className={asistio ? 'asistencia' : 'falta'}
+                  >
+                    {asistio ? 'A' : 'F'}
+                  </td>
+                );
+              })}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default ListaBeneficiarios;
+export default ListaAsistencias;
