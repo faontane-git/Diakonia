7c241f246dfdd620c808977ec6554dce9be15352
Jofrrisc <jofrrisc@espol.edu.ec>
2023-12-19 07:37:39 -0500

    ver asistencias con base de datos

--- diakonia-admin-web/src/components/VerAsistencias.js
+++ diakonia-admin-web/src/components/VerAsistencias.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import Cabecera from "./Cabecera";
 import { Link } from "react-router-dom";
 import '../estilos/VerAsistencias.css'
 
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
 const VerAsistencias = ({ instituciones }) => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const querydb = getFirestore();
+    const queryCollection = collection(querydb, 'instituciones');
+
+    getDocs(queryCollection).then((res) =>
+      setData(res.docs.map((institucion) => ({ id: institucion.id, ...institucion.data() })))
+    );
+  }, []);
+
+  const filteredData = data.filter((institucion) =>
+    institucion.nombre.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+
+
   return (
     <div className="centered-container">
       <Cabecera />
-      <h1>Seguimiento</h1>
+      <h1>Seguimiento asistencias</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Buscar institución..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
       <div className="list-container-verAsistencias">
         <ul id="verAsistencias">
-          {instituciones ? (
-            instituciones.map((institucion) => (
+          {filteredData.length > 0 ? (
+            filteredData.map((institucion) => (
               <li key={institucion.id}>
                 <Link to={`/asistencias/${institucion.id}`}>
                   {institucion.nombre}
