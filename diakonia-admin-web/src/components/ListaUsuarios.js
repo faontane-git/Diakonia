f6788572fda98abb7a27fba44d35a33322959bf1
Jofrrisc <jofrrisc@espol.edu.ec>
2023-12-21 00:59:57 -0500

    actualización usuarios

--- diakonia-admin-web/src/components/ListaUsuarios.js
+++ diakonia-admin-web/src/components/ListaUsuarios.js
@@ -2,7 +2,37 @@ import React from 'react';
 import '../estilos/ListaUsuarios.css';
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
+import { getFirestore, doc, getDoc, updateDoc,deleteDoc } from 'firebase/firestore';
+import firebaseApp from "../firebase-config";
+import {
+  getAuth,
+  deleteUser,
+} from "firebase/auth";
+
 const ListaUsuarios = ({ usuarios }) => {
+
+  async function eliminarUsuario(usuario) {
+    const auth = getAuth();
+    const firestore = getFirestore();
+  
+    try {
+      console.log("entra")
+      // Eliminar el usuario de Firebase Auth
+      //await deleteUser(usuario);
+  
+      // Eliminar el documento del usuario en Firestore
+      //await deleteDoc(doc(firestore, 'usuarios', usuario));
+  
+      // Actualizar el estado de los usuarios (si es necesario)
+      // ...
+  
+    } catch (error) {
+      console.error('Error al eliminar usuario:', error);
+      alert(error.message);
+    }
+  }
+
   return (
     <div className="centered-container">
       <h1>Lista de Usuarios</h1>
@@ -24,7 +54,7 @@ const ListaUsuarios = ({ usuarios }) => {
               <td><Link to={`/editar-usuario/${usuario.id}`}>
                   <button>Editar</button>
                 </Link>
-              <button>Eliminar</button></td>
+                <button onClick={() => eliminarUsuario(usuario.id)}>Eliminar</button></td>
             </tr>
           ))}
         </tbody>
